5deb2da5da98464abc7bb1e8e34aa6377bb9d309
David Leonardo Acosta Laverde <david.acosta.ing@gmail.com>
2022-11-01 09:33:18 -0500

    Score system, menu system

---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private float scoreMultiplier;
+
+    private float _score = 0f;
+
+    public bool ShouldCountScore = true;
+    
+    private void Update()
+    {
+        if (ShouldCountScore)
+        {
+            _score += Time.deltaTime * scoreMultiplier;
+            scoreText.text = Mathf.FloorToInt(_score).ToString();
+        }
+    }
+
+    public int GetScore()
+    {
+        ShouldCountScore = false;
+        return Mathf.FloorToInt(_score);
+    }
+
+    public void RestartScore()
+    {
+        ShouldCountScore = true;
+        _score = 0f;
+        
+    }
+}
